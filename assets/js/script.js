f035af7d9d7f7b3e61f79dad6b3d5904c173085f
Richard <richardyjkim@gmail.com>
2021-02-17 14:46:11 -0800

    taskTypeInput added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,24 @@ let formEl = document.querySelector("#task-form");
 let tasksToDoEl = document.querySelector("#task-to-do");
 
 let createTaskHandler = function(event){
-
   event.preventDefault();
+  let taskNameInput = document.querySelector("input[name='task-name']").value;
+  let taskTypeInput = document.querySelector("select[name='task-type']").value;
+
   
+  // create list item
   let listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
+
+  // create div to hold tas kinfo and add to list item
+  let taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+
+  // add HTML content to div
+  taskInfoEl.innerHTML = "<h3 class'task-name>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  listItemEl.appendChild(taskInfoEl);
+
+  // add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
 };
 
