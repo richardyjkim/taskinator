8d41129a8a31981ffebacd1154d563ad430507d4
Richard <richardyjkim@gmail.com>
2021-02-17 15:22:41 -0800

    add ability to submit custom tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,34 @@
 let formEl = document.querySelector("#task-form");
 let tasksToDoEl = document.querySelector("#task-to-do");
 
-let createTaskHandler = function(event){
+let taskFormHandler = function(event){
   event.preventDefault();
   let taskNameInput = document.querySelector("input[name='task-name']").value;
   let taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+  // check if input values are empty string
+  if (taskNameInput=== "" || taskTypeInput === "") {
+    alert ("You need to fill out the task form!");
+    return false;
+  }
+
+  formEl.reset();
+
+  // reset form fields for next task to be entered
+  document.querySelector("input[name='task-name']").value = "";
+  document.querySelector("select[name='task-type']").secletedIndex = 0;
+  // package up daya as an object
+  let taskDataObj = {
+    name: taskNameInput,
+    type: taskTypeInput
   
-  // create list item
+  };
+
+// send it as an argument to createTaskEl
+createTaskEl(taskDataObj);
+};
+
+let createTaskEl = function(taskDataObj){
   let listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
 
@@ -16,11 +37,10 @@ let createTaskHandler = function(event){
   taskInfoEl.className = "task-info";
 
   // add HTML content to div
-  taskInfoEl.innerHTML = "<h3 class'task-name>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  taskInfoEl.innerHTML = "<h3 class'task-name>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
   listItemEl.appendChild(taskInfoEl);
 
   // add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
-};
-
-formEl.addEventListener("submit", createTaskHandler);
+}
+formEl.addEventListener("submit", taskFormHandler);
